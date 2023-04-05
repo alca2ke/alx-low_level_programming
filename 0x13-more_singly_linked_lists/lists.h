ba90e52386a6398fe8aaedd402b1fd1b65f55fb1
alca2ke <balca5518@gmail.com>
2023-04-05 08:38:06 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -8,7 +9,7 @@
  * @n: integer
  * @next: points to the next node
  *
- Description: singly linked list node structure
+ *Description: singly linked list node structure
  */
 
 typedef struct listint_s
@@ -16,6 +17,20 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
+
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointer
+ */
+
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
