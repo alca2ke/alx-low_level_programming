84e6a0a5e940c4d2ee1692c0f1f78571b676c4f0
alca2ke <balca5518@gmail.com>
2023-04-05 14:08:48 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
 
+/**
+ *malloc_checked - memory allocation
+ *@b:  no of bytes
+ *Return: Returns a pointer to the allocated memory
+ *if malloc fails, status value is equal to 98
+ */
+void *malloc_checked(unsigned int b);
+{
+	char *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+
+	return (p);
+}
